36a26352c9d86147ef2272186194ce97a7303077
tpkolind <tpkolind@gmail.com>
2016-03-06 16:29:18 -0500

    Comment Changes

--- ProductDescription.java
+++ ProductDescription.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Jane
- */
+
+// Class that contains info of a product
 public class ProductDescription {
     
     //variables
--- SalesLineItem.java
+++ SalesLineItem.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Jane
- */
+
+// Class to hold info for a product in the cart
 public class SalesLineItem {
     
     //variables
