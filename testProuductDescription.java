6dc7d3286adaacdcfa3c13a5063c8897128fb4df
jegabbay <gabbay.joey@gmail.com>
2016-04-06 21:42:10 -0400

    Test for Product Description

---
+++ testProuductDescription.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProcessSale;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JGabbay
+ */
+public class testProuductDescription {
+    PersistentStorage storage;
+    String barcode;
+     
+    @Before
+    public void setUp() {
+        storage = PersistentStorage.getInstance();      
+    }
+    
+    @After
+    public void tearDown() {
+        storage.closeConnection();
+    }
+     
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testProductDescription(){
+     System.out.println("Testing Product Description\n");
+     assertNotNull("Description Not Successful", storage.getProductDesc(barcode));
+     assertNotNull("Price Not Successful", storage.getProductPrice(barcode));
+    }
+   } 
