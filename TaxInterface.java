2d2c2700183faca7af297b25102d226f9570b73a
jegabbay <gabbay.joey@gmail.com>
2016-04-11 19:37:24 -0400

    TaxInterface Corrected w/ TA

--- TaxInterface.java
+++ TaxInterface.java
@@ -1,28 +1,17 @@
+package ProcessSale;
+
+import com.Tax.*;
+
 /* John Rogers
 Tax Calculator Interface
 */
 
 
 public class TaxInterface {
-    private static TaxInterface ti = null;
-    private TaxCalculator taxCalc;
-
-    private TaxInterface(){
-	taxCalc = new TaxCalculator();
-    }
-
-    public static TaxInterface getInstance(){
-	if(ti == null){
-	    ti = new TaxInterface();
-	    return ti;
-	}
-	else
-	    return ti;
-    }
-
-    public static double getTax(double subtotal){
-	double tax = ti.calculateTax(subtotal);
+    private static TaxCalculator taxCalc = TaxCalculator.getInstance();
+    
+    public static double getTax(double subtotal) {
+	double tax = taxCalc.calculateTax(subtotal);
 	return tax;
     }
-
 }
